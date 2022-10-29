a191092fd1a8a40948d4bcdfbe2d09859fe86977
seonhannn <seon.hannn@gmail.com>
2022-10-29 23:25:53 +0900

    head, container

---
+++ src/components/TodoTemplate.js
@@ -0,0 +1,26 @@
+import React from "react";
+import styled from "styled-components";
+
+const TodoTemplateBlock = styled.div`
+    width: 500px;
+    height: 600px;
+    position: relative;
+    background: white;
+    border-radius: 16px;
+    box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.04);
+    margin: 0 auto;
+    margin-top: 70px;
+    margin-bottom: 30px;
+    display: flex;
+    flex-direction: column;
+`;
+
+function TodoTemplate({children}) {
+    return (
+        <TodoTemplateBlock>
+            {children}
+        </TodoTemplateBlock>
+    );
+}
+
+export default TodoTemplate;
